8beed23e6f125a87ea79d88d945f22f58650878a
estef.space <estef.space@gmail.com>
2024-10-02 04:10:29 +0000

    changeTheme, favicon fix

--- src/scripts/changeTheme.js
+++ src/scripts/changeTheme.js
@@ -1,13 +1,19 @@
 
 function darkTheme() {
     document.querySelector("html").setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
     document.getElementById("icon-theme").setAttribute("class", "fa-solid fa-sun");
 }
 
 function lightTheme() {
     document.querySelector("html").setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
     document.getElementById("icon-theme").setAttribute("class", "fa-solid fa-moon");
 }
+
+const theme = localStorage.getItem("theme");
+
+theme === 'dark' ? darkTheme() : lightTheme();
 
 const changeTheme = document.getElementById("change-theme");
 
