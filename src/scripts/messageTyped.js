ba75ea60689dd28844068788cd5e357f36105cc4
estef.space <estef.space@gmail.com>
2024-10-03 04:41:42 +0000

    new typed messages

--- src/scripts/messageTyped.js
+++ src/scripts/messageTyped.js
@@ -1,6 +1,6 @@
 import Typed from 'typed.js';
 
 const typed = new Typed('.typed', {
-  strings: ['pajero', 'inteligente', 'huevon', 'guapo', 'feo', 'malo', 'futbolista'],
+  strings: ['pajero', 'inteligente', 'huevon', 'guapo', 'deportista', 'defensa', 'futbolista'],
   typeSpeed: 50,
 });
