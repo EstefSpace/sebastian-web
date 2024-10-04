ad92eb049a5f3824bab8edffcf62577c21d8caac
estef.space <estef.space@gmail.com>
2024-10-04 02:59:04 +0000

    bucle messageTyped.js

--- src/scripts/messageTyped.js
+++ src/scripts/messageTyped.js
@@ -3,4 +3,5 @@ import Typed from 'typed.js';
 const typed = new Typed('.typed', {
   strings: ['pajero', '', 'inteligente', '',  'huevon', '', 'guapo', '', 'cachondo', '', 'sexy', '', 'mamon', '',  'futbolista'],
   typeSpeed: 80,
+  loop: true
 });
